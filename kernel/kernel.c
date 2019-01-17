cb8dbc585d5178c66b66f5b669825bad45e63f98
Muhammad Quwais Safutra <45951646+BRAKOMANDA@users.noreply.github.com>
2019-01-18 04:42:31 +0700

    Create kernel.c

---
+++ kernel/kernel.c
@@ -0,0 +1,25 @@
+/* kernel.c
+*  special thanks to : Muhammad Quwais Safutra
+*  Kernel Ini Akan Membuat Semua Berjalan dengan mudah
+*/
+
+#include <stdio.h>
+#include <_mingw.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+int main(){
+   __cdelcspec static unsigned 0x006
+   struct x86;
+   if(!NULL)
+      __wxcdels unsigned short;
+   else
+      __asm64 long long* main(**kwargs);
+   setup->NULL;
+   try {
+      set __asm64 && __asm86x -> setup;
+      setup -> return;
+   } catch (Exception const,s){
+      __resources static malloc*-7;
+   }
+}       
